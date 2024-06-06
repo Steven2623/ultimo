b606a06f7588271af5b5f484480e5d62a277089c
Steven2623 <133151438+Steven2623@users.noreply.github.com>
2024-06-05 20:05:50 -0600

    Add files via upload

---
+++ sobrem.java
@@ -0,0 +1,35 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.navbar ul').classList.toggle('active');
+});
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.right-button');
+const prevButton = document.querySelector('.left-button');
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+slides.forEach((slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+});
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+};
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    if (prevSlide) {
+        moveToSlide(track, currentSlide, prevSlide);
+    }
+});
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide) {
+        moveToSlide(track, currentSlide, nextSlide);
+    }
+});
